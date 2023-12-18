64c86a469d936b75b2a4a8eaf55bbaa63f06d256
AlexGuojl <109111350+AlexGuojl@users.noreply.github.com>
2023-12-18 13:51:58 +0800

    Update and rename bootstrap_analysis_for_HPC(ready_to_submit).py to bootstrap_analysis_for_ED_estimation.py

--- bootstrap_analysis_for_ED_estimation.py
+++ bootstrap_analysis_for_ED_estimation.py
@@ -4,12 +4,16 @@ import os
 import random
 from random import choice
 import time
-
+from datetime import datetime
+from tqdm import tqdm
+tqdm.pandas(desc='apply')
 os.chdir("/Users/alexgjl/Desktop/master/项目2/文件")##change this to the path that the files existed in your computer
 
+
 #read in leaves table and keep necessary columns. 
-df_leaves = pd.read_csv("ordered_leaves.csv",low_memory=False)
-leaves1 = pd.DataFrame(df_leaves,columns = ["id","parent","ott"])
+
+df_leaves = pd.read_csv("updated_ordered_leaves_2.0.csv",low_memory=False)
+leaves1 = pd.DataFrame(df_leaves,columns = ["id","parent","ott","real_parent"])
 
 
 #leaves table for calculating ed(leaves2 is a table of leaves in which species that have the same most
@@ -18,12 +22,22 @@ leaves1 = pd.DataFrame(df_leaves,columns = ["id","parent","ott"])
 leaves2 = leaves1
 leaves2['id'] = leaves2['id'].astype(str)
 leaves2 = leaves2.groupby("parent")["id"].apply(lambda x:x.str.cat(sep = ",")).reset_index()
+new_id_ls = []
+for row in leaves2.itertuples():
+    leaf_id = getattr(row,"id")#can be 1 or a list of node date estimates
+    if  ","  in leaf_id:
+        ls_leaf_id = leaf_id.split(",")
+        leaf_id = int(ls_leaf_id[0])
+        new_id_ls.append(leaf_id)
+    else:
+        leaf_id = int(leaf_id)
+        new_id_ls.append(leaf_id)
 
-
+leaves2["new_id"] = new_id_ls
 #read in nodes table.
 
-    #nodes for finding parents
-df_nodes = pd.read_csv("ordered_nodes.csv",low_memory=False)
+#nodes for finding parents
+df_nodes = pd.read_csv("updated_ordered_nodes_2.0.csv",low_memory=False)
 nodes1 = pd.DataFrame(df_nodes,columns = ["id","ott","parent","real_parent","node_rgt","leaf_lft","leaf_rgt","age"])
 
     #nodes for calculating ED
@@ -31,28 +45,19 @@ nodes = pd.DataFrame(nodes1, columns = ["Unnamed: 0","id","parent","leaf_lft","l
 nodes = nodes.fillna(0)
 
 
-def find_node_parents(a):##find parents based on a node id(a)
-    list_parentsn = []
-    cp = nodes1.iat[int(a)-1,2]#current parents
-    while cp != -27400288:
-        list_parentsn.append(cp)
-        cp = nodes1.iat[int(cp)-1,2]     
-    return sorted(list_parentsn)
-
-
-def find_closest_parents_with_date(a):##find closest node parents that have a date
+def find_parents_with_date(node_id):##return a list of date
     #based on a node id(a)
-    cp = nodes1.iat[int(a)-1,2]#current parents
-    while cp != -27400288:#not the first node
-        #once cp in ages_selected，which means this id has a date
-        if cp in ages_selected_with_parent["id"].values:
-            date_estimate = find_age(cp)
-            return(date_estimate)
-        else:
-            cp = nodes1.iat[int(cp)-1,2]
-            #will return 4025 if no parents have age
-
-
+    list_try = []#list of parents
+    cp = df_nodes.iat[int(node_id)-1,2]
+    while cp != -27400288:##not the root(oldest node)
+        list_try.append(cp)
+        cp = df_nodes.iat[int(cp)-1,2]
+        #from young to old
+    list_try.append(node_id)
+    list_try = sorted(list_try,reverse = True)
+    #list_try = sorted(list(set(ls_realp_node) & set(list_try)),reverse = True)#list of real parent
+    list_date = [find_age(i) for i in list_try]
+    return([i for i in list_date if i > 0])
 
 def find_des(a): ##return 1/descendants for a node id in the whole table
     if a == -1:
@@ -75,6 +80,7 @@ def ed_new(leaf_id):
         list_try.append(cp)
         cp = nodes.iat[int(cp)-1,2]
     list_try = sorted(list_try,reverse = True)
+    #print(list_try)
     #list_try is a list of parents sorted from young to old 
     ls_des = []
     ls_part = []
@@ -94,26 +100,29 @@ def ed_new(leaf_id):
             ind += 1
             ls_part = []
             ls_des = []
+        #print(ls_des)
+        #print(ls_part)
     return(float(format(sum(ls_ed),'.6f')))
 
 
 
-
-
-#an incomplete node table that will be merged with age table later
+##has been updated with the latest json data
 nodes_no_age = pd.DataFrame(nodes,columns = ["Unnamed: 0","id","parent","leaf_lft","leaf_rgt","unnamed:0"])
 
-##has been updated with the latest json data
-ages = pd.read_csv("latest_node_dates.csv", low_memory=False)
+##always been updated with the latest json data
+ages = pd.read_csv("latest_node_dates(real_parent)_2.0.csv", low_memory=False)
+
 
 
 list_of_ed_list = []
 ##main program(return a 1656825 ed scores list)
 
-times = 0
-while times < 100:
+times = 1
+while times < 101:
     start_time = time.time()
     random.seed(times)
+    currentDateAndTime = datetime.now()
+   # print(currentDateAndTime.strftime("%H:%M:%S")) 
     ages_selected = ages.sample(frac=0.5, replace=False, random_state = None)
     #choose one if a node has several date estimates
     ls_age  = []
@@ -125,67 +134,62 @@ while times < 100:
             ls_age.append(age_selected)
         else:#this selected node have only 1 date
             ls_age.append(age)
+
     ages_selected['age'] = ls_age#add a column called age
+    ages_selected["age"] = pd.to_numeric(ages_selected["age"])
+    
     ages_selected = pd.DataFrame(ages_selected,columns = ['id', 'age'])
+    
     ages_bootstrap_final = pd.merge(nodes_no_age, ages_selected, how = "left",on = "id")
     ages_bootstrap_final = ages_bootstrap_final.fillna(0)
     ages_bootstrap_final.iat[0,6] = 4025.0#give age to the root/node1, always here
     ages_bootstrap_final["age"] = pd.to_numeric(ages_bootstrap_final["age"])
 
-
-##find parent for nodes in ages_selected
-    ages_selected_with_parent= pd.merge(nodes_no_age,
-                                    ages_selected, how = "left",on = "id")
-    ages_selected_with_parent.iat[0,6] = 4025.0
-    ages_selected_with_parent["age"] = pd.to_numeric(ages_selected_with_parent["age"])
-    ages_selected_with_parent = ages_selected_with_parent[ages_selected_with_parent["age"]>0]
-
-
-##see whether there is descendants that has a larger age than parents
-    #randomnize the order
-    ages_selected_reindexed = ages_selected.reindex(np.random.permutation(ages_selected.index))
-    for row in ages_selected_reindexed.itertuples():
-        node_id = getattr(row,"id")
-        age_p = find_closest_parents_with_date(node_id)##return the date estimate of its closest parent
-        node_age = eval(getattr(row,"age"))#age of itself
-        if node_age > age_p:
-            ls_select = []
-            ls_select.append(age_p)
-            ls_select.append(node_age)
-            selected = choice(ls_select)#select one age to discard
-            ages_bootstrap_final["age"].replace(selected, 0)#the age of discarded node is 0     
-
-##combine the two species that have the same most recent common ancestor,可优化
-    new_id_ls = []
-    for row in leaves2.itertuples():
-        leaf_id = getattr(row,"id")#can be 1 or a list of node date estimates
-        if  ","  in leaf_id:
-            ls_leaf_id = leaf_id.split(",")
-            leaf_id = int(ls_leaf_id[0])
-            new_id_ls.append(leaf_id)
+    ##see whether there is descendants that has a larger age than parents
+    for row in ages_bootstrap_final[ages_bootstrap_final["age"]>0][1:].itertuples():
+        node_id = int(getattr(row,"id"))
+        lsdates =  find_parents_with_date(node_id)
+        lsdates1 = sorted(lsdates)
+        if lsdates == lsdates1:
+            continue
         else:
-            leaf_id = int(leaf_id)
-            new_id_ls.append(leaf_id)
+            i = 0
+            ls_chose = []
+            while i < len(lsdates):
+                for j in range(1+i,len(lsdates)):
+                    if lsdates[i] <= lsdates[j]:
+                        continue
+                    if lsdates[i]>lsdates[j]:
+                        ls_chose.append(lsdates[i])
+                        ls_chose.append(lsdates[j])
+                        selected = choice(ls_chose)
+                        if selected == lsdates[j]:#find the index,replace it in nodes table
+                            inds =  ages_bootstrap_final[ ages_bootstrap_final.age == selected].index.tolist()
+                            for k in inds:
+                                 ages_bootstrap_final.at[k,"age"] = 0
+                            ls_chose = []
+                        if selected == lsdates[i]:#find the index,replace it in nodes table
+                            inds =  ages_bootstrap_final[ ages_bootstrap_final.age == selected].index.tolist()
+                            for k in inds:
+                                 ages_bootstrap_final.at[k,"age"] = 0
+                            ls_chose = []
+                i+=1
+##combine the two species that have the same most recent common ancestor
+#leaves table for calculating ed(leaves2 is a table of leaves in which species that have the same real_parent are combined)
 
     ##ccalculate ed for the entire table###############################
-    leaves2["new_id"] = new_id_ls
     ed_total = leaves2["new_id"].apply(ed_new)
     #get the end time
     times = times+1
     end_time = time.time()
     print(times)
     print((end_time-start_time)/3600)
+    print(currentDateAndTime.strftime("%H:%M:%S"))
     list_of_ed_list.append(ed_total)
 
 df_ed = pd.DataFrame(list_of_ed_list).transpose()
-##a dataframe of ed scores
+df_ed.to_csv("bootstraped_ed(1_100).csv",encoding = "gbk")
 
-#df_ed.to_csv("df_ed.csv",encoding = "gbk")
-
-df_ed.to_csv("ed_bootstrap(100).csv",encoding = "gbk")
-
-
-##
 
 
 

222dd4fc31fc6ab84f4a2e20ecaad972ffdeb52a
AlexGuojl <109111350+AlexGuojl@users.noreply.github.com>
2024-03-27 18:50:15 +0800

    Update bootstrap_analysis_for_ED_estimation.py

--- bootstrap_analysis_for_ED_estimation.py
+++ bootstrap_analysis_for_ED_estimation.py
@@ -185,7 +185,22 @@ while times < 101:
     list_of_ed_list.append(ed_total)
 
 df_ed = pd.DataFrame(list_of_ed_list).transpose()
+
+
+df_ed0 = df_ed
+df_ed["ed"] = df_ed0.median(axis=1)
+df_ed["sum_ed"] = list(df_ed0.apply(lambda x: x.sum(), axis = 1))
+leaves2 = leaves1
+leaves2['id'] = leaves2['id'].astype(str)
+leaves2 = leaves2.groupby("parent")["id"].apply(lambda x:x.str.cat(sep = ",")).reset_index()
+df_ed["parent"]=leaves2["parent"]#到这里
+leaves_with_ed = pd.merge(leaves1,df_ed,how = "left",on = "parent")
+df_median_ed = pd.DataFrame(df_ed,columns = ["id","parent","ed"])
+
+df_median_ed.to_csv("median_ed_final.csv",encoding = "gbk")
 df_ed.to_csv("bootstraped_ed(1_100).csv",encoding = "gbk")
+
+
 
 
 

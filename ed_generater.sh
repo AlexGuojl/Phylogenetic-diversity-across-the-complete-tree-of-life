675f740a5caaa9628651cf614b7795abfd1189cc
AlexGuojl <109111350+AlexGuojl@users.noreply.github.com>
2025-12-12 17:22:21 +0800

    Update ed_generater.sh

--- ed_generater.sh
+++ ed_generater.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=1:mem=5gb
+#PBS -l select=1:ncpus=1:mem=10gb
 #PBS -l walltime=00:10:00
 #PBS -N generate_shell
 
@@ -11,13 +11,17 @@ module load SciPy-bundle/2022.05-foss-2022a
 #cp -r ed_data_collection/\$arg_value* \$TMPDIR/ed_data_collection/
 #cd \$TMPDIR/ed_data_collection
 
-for i in {1..1000}; #replace the number here with random_seeds
+
+
+for i in {1..1000}; 
 do
+
     filename="full${i}.sh"  
+    
     cat << EOF > ${filename}
 #!/bin/bash
-#PBS -l select=1:ncpus=1:mem=50gb
-#PBS -l walltime=50:00:00
+#PBS -l select=1:ncpus=12:mem=50gb
+#PBS -l walltime=7:00:00
 #PBS -N ED_estimation_${i}
 #PBS -o ED_estimation_${i}.out
 #PBS -e ED_estimation_${i}.err
@@ -29,9 +33,12 @@ module load tools/prod
 module load SciPy-bundle/2022.05-foss-2022a
 
 
+# set arg_value
 arg_value=${i}
 
-python3 ED_driver.py --arg1 "\$arg_value" > "\$arg_value.part"
+# 调用 Python 脚本
+
+/usr/bin/time -v python3 ED_driver_3.0_12core.py  --arg1 "\$arg_value" > "\$arg_value.part" 
 
 wait
 
